9d7fab4783e367239d2e95e004053f30b0a0bf0a
baothi <baothi246@gmail.com>
2020-12-11 16:00:46 +0700

    magento

---
+++ generated/code/Magento/Sales/Api/Data/OrderExtensionInterface.php
@@ -0,0 +1,9 @@
+<?php
+namespace Magento\Sales\Api\Data;
+
+/**
+ * ExtensionInterface class for @see \Magento\Sales\Api\Data\OrderInterface
+ */
+interface OrderExtensionInterface extends \Magento\Framework\Api\ExtensionAttributesInterface
+{
+}
